91e466946a7a276f9325f7471e28c66b452eca4a
PavelMarishev <karthusss@ya.ru>
2017-10-26 16:26:12 +0300

    Got instance from context

--- src/main/java/com/teaminternational/web/HttpServlet.java
+++ src/main/java/com/teaminternational/web/HttpServlet.java
@@ -1,5 +1,26 @@
 package com.teaminternational.web;
 
+import com.teaminternational.HelloWorldGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HttpServlet extends BaseHttpServlet{
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        super.doGet(req, resp);
+        HelloWorldGenerator hello = getBean(HelloWorldGenerator.COMPONENT_NAME);
+        hello.sayHello();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        super.doPost(req, resp);
+    }
 }
