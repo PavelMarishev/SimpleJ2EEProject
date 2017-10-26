ab95d4e4ca915dc65f8e04c0e5f4a360137f713b
PavelMarishev <karthusss@ya.ru>
2017-10-26 16:56:40 +0300

    Helloworld

--- src/main/java/com/teaminternational/web/HttpServlet.java
+++ src/main/java/com/teaminternational/web/HttpServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HttpServlet extends BaseHttpServlet{
 
@@ -15,6 +16,18 @@ public class HttpServlet extends BaseHttpServlet{
         super.doGet(req, resp);
         HelloWorldGenerator hello = getBean(HelloWorldGenerator.COMPONENT_NAME);
         hello.sayHello();
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+
+        pw.print("<html>\n" +
+                "<head>\n" +
+                "<title>HelloWorldPage</title>\n" +
+                "/head\n" +
+                "body bgcolor=\"grey\"\n" +
+               hello.sayHello()+"\n"+
+                "</body>\n"+
+                "</html>\n");
+
     }
 
     @Override
